3463c792c9c8f94bf7f4b3168330cf5be4664668
MUSHUBO <shubo3132@gmail.com>
2025-03-17 15:16:53 +0600

    added the lession button accessing some work

--- scripts/index.js
+++ scripts/index.js
@@ -1,2 +1,33 @@
-console.log("I am connected")
+console.log("I am connected");
 
+function vocabularyBtn() {
+    fetch("https://openapi.programming-hero.com/api/levels/all")
+        .then((response) => response.json())
+        .then((data) => displayData(data.data))
+}
+
+// {
+//     "id": 101,
+//     "level_no": 1,
+//     "lessonName": "Basic Vocabulary"
+// }
+
+function displayData(data) {
+    console.log(data);
+    // access all btn
+    const buttonContainer = document.getElementById('button-container')
+    
+    // api all data
+    for (let btnData of data) {
+        const btnDiv = document.createElement('div')
+        btnDiv.innerHTML = `
+         <button id="" class="btn btn-outline btn-primary">
+         <i class="fa-solid fa-book-open"></i> Lesson -${btnData.level_no}</button>
+        `;
+
+        buttonContainer.append(btnDiv);
+    }
+}
+
+
+vocabularyBtn();
