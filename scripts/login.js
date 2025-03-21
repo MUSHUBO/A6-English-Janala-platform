d86146e266af055deedd798b0f4a9b2b3f7c4086
MUSHUBO <shubo3132@gmail.com>
2025-03-21 07:11:51 +0600

    added the hero section login form

--- scripts/login.js
+++ scripts/login.js
@@ -1,6 +1,33 @@
-console.log('login add')
+console.log('login add');
 
-const loginBtn = document.getElementById('login-btn')
+
+const loginBtn = document.getElementById("login-btn")
     .addEventListener('click', function () {
+        // console.log("hello btn");
+
+        const loginName = document.getElementById("login-name").value;
+        // console.log(loginName);
+
+        const loginPassword = document.getElementById("login-password").value;
+        const convertedPassword = parseInt(loginPassword);
+        console.log(loginName, convertedPassword);
+
+        if (loginName.length != 0) {
+            if (convertedPassword === 123456) {
+                document.getElementById("navbar-container").classList.add("hidden");
+                document.getElementById("navbar-img").classList.add("hidden");
+
+                document.getElementById("navbar").classList.remove("hidden");
+                document.getElementById("section1").classList.remove("hidden");
+                document.getElementById("section2").classList.remove("hidden");
+                document.getElementById("footer").classList.remove("hidden");
+                
+            } else {
+                alert('Wrong Password. Contact admin to get you Login code.')
+            }
+        } else {
+            alert('Please tell your Name first.')
+        }
+
 
     })
--- scripts/scrolling.js
+++ scripts/scrolling.js
@@ -1,9 +1,9 @@
 console.log('scrolling connected')
 
 document.getElementById("btn1").addEventListener("click", function () {
-    document.getElementById("section1").scrollIntoView({ behavior: "smooth" });
+    document.getElementById("section2").scrollIntoView({ behavior: "smooth" });
 });
 
 document.getElementById("btn2").addEventListener("click", function () {
-    document.getElementById("section2").scrollIntoView({ behavior: "smooth" });
+    document.getElementById("section1").scrollIntoView({ behavior: "smooth" });
 });
